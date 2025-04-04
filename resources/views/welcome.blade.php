ca676cb03ebb55238e726220b3837d1a22738ad9
orcrobert <robyorban@gmail.com>
2025-04-04 15:26:15 +0300

    pass var to view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
     <h1>Home page</h1>
+    {{ $eu }}
+    <br>
+    {{ $tu }}
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'eu' => 'robert',
+        'tu' => 'mata',
+    ]);
 });
 
 Route::get('/about', function() {
