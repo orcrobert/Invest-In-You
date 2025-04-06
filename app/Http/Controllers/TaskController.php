d94475368112c56bf0929a5840df1bea5b496f03
orcrobert <robyorban@gmail.com>
2025-04-06 12:17:28 +0300

    created a task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index() {
+        $tasks = Task::with('category')->latest()->paginate(3);
+
+        return view('task.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function create() {
+        return view('task.create');
+    }
+
+    public function show($id) {
+        $task = Task::find($id);
+        return view('task.show', 
+        ['task' => $task]);
+    }
+
+    public function store() {
+        request()->validate([
+            'title' => ['required', 'string', 'min:5'],
+            'description' => ['string', 'min:10'],
+            'deadline' => ['date', 'nullable'],
+        ]);
+    
+        Task::create([
+            'title'=> request('title'),
+            'description'=> request('description'),
+            'deadline' => request('deadline'),
+            'category_id' => 1,
+        ]);
+    
+        return redirect('/tasks'); 
+    }
+
+    public function edit($id) {
+        $task = Task::find($id);
+        return view('task.edit', ['task' => $task]);
+    }
+
+    public function update($id) {
+        request()->validate([
+            'title' => ['required', 'string', 'min:5'],
+            'description' => ['string', 'min:10'],
+            'deadline' => ['date', 'nullable'],
+        ]);
+    
+        $task = Task::findOrFail($id);
+        $task->update([
+            'title' => request('title'),
+            'description' => request('description'),
+            'deadline' => request('deadline'),
+            'category_id' => 1,
+        ]);
+    
+        return redirect('/tasks');
+    }
+
+    public function destroy($id) {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return redirect('/tasks');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,85 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Task;
-
+use App\Http\Controllers;
+use App\Http\Controllers\TaskController;
 
 Route::get("/", function () {
     return view("home");
-});
-
-Route::get('/tasks', function () {
-    $tasks = Task::with('category')->latest()->paginate(3);
-
-    return view('task.index', [
-        'tasks' => $tasks,
-    ]);
-});
-
-Route::get('/task/{id}', function ($id) {
-    $task = Task::find($id);
-    return view('task.show', 
-    ['task' => $task]);
 });
 
 Route::get('/about', function() {
     return view('about');
 });
 
-Route::get('/create', function () {
-    return view('task.create');
-});
-
 Route::get('/contact', function() {
     return view('contact');
 });
 
-Route::post('/tasks', function() {
+Route::get('/tasks', [TaskController::class, 'index']);
 
-    request()->validate([
-        'title' => ['required', 'string', 'min:5'],
-        'description' => ['string', 'min:10'],
-        'deadline' => ['date', 'nullable'],
-    ]);
+Route::get('/task/{id}', [TaskController::class, 'show']);
 
-    Task::create([
-        'title'=> request('title'),
-        'description'=> request('description'),
-        'deadline' => request('deadline'),
-        'category_id' => 1,
-    ]);
+Route::get('/create', [TaskController::class, 'create']);
 
-    return redirect('/tasks');
-});
+Route::post('/tasks', [TaskController::class, 'store']);
 
-// Edit
-Route::get('/task/{id}/edit', function ($id) {
-    $task = Task::find($id);
-    return view('task.edit', ['task' => $task]);
-});
+Route::get('/task/{id}/edit', [TaskController::class, 'edit']);
 
-// Update
-Route::patch('/task/{id}', function ($id) {
-    request()->validate([
-        'title' => ['required', 'string', 'min:5'],
-        'description' => ['string', 'min:10'],
-        'deadline' => ['date', 'nullable'],
-    ]);
+Route::patch('/task/{id}', [TaskController::class, 'update']);
 
-    $task = Task::findOrFail($id);
-    $task->update([
-        'title' => request('title'),
-        'description' => request('description'),
-        'deadline' => request('deadline'),
-        'category_id' => 1,
-    ]);
-
-    return redirect('/tasks');
-});
-
-// Delete
-Route::delete('/task/{id}', function ($id) {
-    $task = Task::findOrFail($id);
-    $task->delete();
-
-    return redirect('/tasks');
-});
+Route::delete('/task/{id}', [TaskController::class, 'destroy']);
