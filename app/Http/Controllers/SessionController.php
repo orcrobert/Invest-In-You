a13641d680a7403f4d6d8490ef5dd95a27526d92
orcrobert <robyorban@gmail.com>
2025-04-06 17:21:23 +0300

    login error

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -19,7 +20,11 @@ class SessionController extends Controller
             'password' => ['required',]
         ]);
 
-        Auth::attempt($attributes);
+        if (!Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'error' => 'Credentials do not match'
+            ]);
+        } 
 
         request()->session()->regenerate();
         return redirect('/tasks');
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,6 +31,10 @@
                             class="border-2 border-gray-800 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                     </div>
                 </div>
+                
+                @error('error')
+                    <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                @enderror
 
                 <div>
                     <button type="submit"
