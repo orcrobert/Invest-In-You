acf8a9086dfe950454fdaeca087df5534416503b
orcrobert <robyorban@gmail.com>
2025-04-12 19:51:32 +0300

    yaman

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -6,7 +6,7 @@
             </a>
         @else
             <a href="/tasks?status=not-completed" class="inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-                Show not completed Tasks
+                Show Pending Tasks
             </a>
         @endif
     </div>
