f727d95313842e1e90061919d2ce54244c762ec0
onigaaris <onigaaris50@gmail.com>
2025-04-12 20:36:43 +0300

    updated

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -84,7 +84,7 @@
                                                 <svg class="w-4 h-4 inline-block mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
                                                 </svg>
-                                                Deadline: {{ $task->deadline ? $task->deadline->format('d.m.Y H:i') : 'Nespecificat' }}
+                                                Deadline: {{ $task->deadline ? \Carbon\Carbon::parse($task->deadline)->format('d.m.Y H:i') : 'Nespecificat' }}
                                             </span>
                                             <span class="text-sm text-gray-500">
                                                 <svg class="w-4 h-4 inline-block mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
